5fef902fa5c006cbce2a31c63f7cb746fff090bb
James Grant <jgrant@James-MacBook-Pro.local>
2017-10-16 15:28:16 +0100

    working build

---
+++ src/main/java/digital/and/demo/model/User.java
@@ -0,0 +1,59 @@
+package digital.and.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+    private String nationality;
+    private String age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/digital/and/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package digital.and.demo.repository;
+
+import digital.and.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface UserRepository extends JpaRepository<User, Long> {
+}

9160b7aaa93a9027f84dfb172d85a7b328bb35f1
jhgrant <jgrant@and.digital>
2017-10-17 11:13:12 +0100

    integration tests added

--- src/main/java/digital/and/demo/repository/UserRepository.java
+++ src/main/java/digital/and/demo/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    public User findByFirstname(String name);
 }

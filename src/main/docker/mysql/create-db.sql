1a093f011a729f93bd61055ead2e4767dff62f28
jhgrant <jgrant@and.digital>
2017-10-17 17:01:17 +0100

    latest code - not fully working

--- src/main/docker/mysql/create-db.sql
+++ src/main/docker/mysql/create-db.sql
@@ -1,4 +1,7 @@
+CREATE USER 'demo'@'localhost' IDENTIFIED BY 'demo';
 CREATE USER 'demo'@'%' IDENTIFIED BY 'demo';
+
+GRANT ALL PRIVILEGES ON * . * TO 'demo'@'localhost';
 GRANT ALL PRIVILEGES ON * . * TO 'demo'@'%';
 FLUSH PRIVILEGES;
 

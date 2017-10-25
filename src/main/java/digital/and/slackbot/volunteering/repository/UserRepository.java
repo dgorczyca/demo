1604506f75787b3d69bd917b949dde8e9cf2f297
Dawid Gorczyca <dgorczyca@and.digital>
2017-10-25 17:18:41 +0100

    Adding user + linking them with the event

--- src/main/java/digital/and/slackbot/volunteering/repository/UserRepository.java
+++ src/main/java/digital/and/slackbot/volunteering/repository/UserRepository.java
@@ -2,9 +2,9 @@ package digital.and.slackbot.volunteering.repository;
 
 import digital.and.slackbot.volunteering.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     public User findBySlackUsername(String name);
